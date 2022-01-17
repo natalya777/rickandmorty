6f9e705ef4720c0a4ebd032cf0235c5a828fc4b9
Natalya Ryabinina <256611@mail.ru>
2022-01-17 20:23:44 +0300

    search_bloc1

---
+++ lib/feature/presentation/block/search_bloc/search_state.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+
+import 'package:equatable/equatable.dart';
+import 'package:rickandmorty/feature/domain/entities/person_entity.dart';
+
+abstract class PersonSearchState extends Equatable {
+  const PersonSearchState();
+  @override
+  List<Object> get props => [];
+}
+
+class PersonEmpty extends PersonSearchState {}
+
+class PersonSearchLoading extends PersonSearchState {}
+
+class PersonSearchLoaded extends PersonSearchState {
+  final List<PersonEntity> persons;
+
+  PersonSearchLoaded({required this.persons});
+  @override
+  List<Object> get props => [persons];
+}
+
+class PersonSearchError extends PersonSearchState {
+  final String message;
+
+  PersonSearchError({required this.message});
+  @override
+  // TODO: implement props
+  List<Object> get props => [message];
+}
